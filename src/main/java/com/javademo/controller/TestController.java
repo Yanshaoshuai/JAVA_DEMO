5ca0770dc0a4a5178c75b024c417f71969cb95ab
yanshaoshuai <ahutyss@gmail.com>
2022-06-22 23:48:11 +0800

    https test1

---
+++ src/main/java/com/javademo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.javademo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
