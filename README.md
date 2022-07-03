c2223fa9998f0afde4879827b7ebdff2c0322396
yanshaoshuai <ahutyss@gmail.com>
2022-07-03 19:20:25 +0800

    add todo and change JobApp

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # JAVA_DEMO
 record some java demo code
+
+## api
+### spring controller crud
+### oauth客户端
+### jwt
+
+## todo
+oauth 客户端服务端
+okhttp util
+date util
+code gen
+ssl
+es
+redis
+
--- job/src/main/java/com/javademo/job/JobApp.java
+++ job/src/main/java/com/javademo/job/JobApp.java
@@ -1,14 +1,11 @@
 package com.javademo.job;
 
-import com.javademo.common.utils.ExecScriptUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class JobApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         SpringApplication.run(JobApp.class,args);
     }
 }
