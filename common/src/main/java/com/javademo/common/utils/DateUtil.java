b220d36e74d60aca53e86abd977725a90f7f7cb6
yanshaoshuai <ahutyss@gmail.com>
2022-07-06 19:14:49 +0800

    add some test

--- common/src/main/java/com/javademo/common/utils/DateUtil.java
+++ common/src/main/java/com/javademo/common/utils/DateUtil.java
@@ -11,14 +11,22 @@ import java.util.stream.Collectors;
 public class DateUtil {
     private DateUtil(){}
     /**
-     * UTC时间格式
+     * ISO时间格式
+     * 2022-07-06T18:21:34.499Z
      */
-    public static final DateTimeFormatter UTC_FORMATTER=DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    public static final DateTimeFormatter ISO_FORMATTER=DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
     /**
      * 普通时间格式
+     * 2022-07-06 18:15:29:410
      */
     public static final DateTimeFormatter COMMON_FORMATTER=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS");
+
+    /**
+     * 时区时间格式
+     * 2022-07-06 18:15:29:410 GMT+08:00/2022-07-06 18:15:29:410 HKT
+     */
+    public static final DateTimeFormatter ZONE_TIME_FORMAT=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS z");
 
     /**
      * 获取所有时区
@@ -30,13 +38,13 @@ public class DateUtil {
     }
 
     /**
-     * 格式转为UTC
+     * 格式转为ISO格式
      * @param dateTimeStr
      * @param formatter
      * @return
      */
-    public static String getUtcFormatDateTime(String dateTimeStr,DateTimeFormatter formatter){
-        return LocalDateTime.parse(dateTimeStr,formatter).format(UTC_FORMATTER);
+    public static String getISOFormatDateTime(String dateTimeStr,DateTimeFormatter formatter){
+        return LocalDateTime.parse(dateTimeStr,formatter).format(ISO_FORMATTER);
     }
 
     /**
@@ -47,9 +55,9 @@ public class DateUtil {
      * @param formatter
      * @return
      */
-    public static String zoneConvert(String dateTimeStr,String sourceZone,String targetZone,DateTimeFormatter formatter){
-        ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.parse(dateTimeStr, formatter), ZoneId.of(sourceZone));
-        ZonedDateTime targetZoneDateTime = zonedDateTime.withZoneSameInstant(ZoneId.of(targetZone));
+    public static String zoneConvert(String dateTimeStr,ZoneId sourceZone,ZoneId targetZone,DateTimeFormatter formatter){
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.parse(dateTimeStr, formatter),sourceZone);
+        ZonedDateTime targetZoneDateTime = zonedDateTime.withZoneSameInstant(targetZone);
         return targetZoneDateTime.format(formatter);
     }
 
@@ -60,16 +68,12 @@ public class DateUtil {
      * @param formatter
      * @return
      */
-   public static String zoneToUTC(String dateTimeStr,String sourceZone,DateTimeFormatter formatter){
-        return zoneConvert(dateTimeStr,sourceZone,"UTC",formatter);
+   public static String zoneToUTC(String dateTimeStr,ZoneId sourceZone,DateTimeFormatter formatter){
+        return zoneConvert(dateTimeStr,sourceZone,ZoneId.of("UTC"),formatter);
    }
 
-    public static void main(String[] args) {
-        List<String> allTimeZones = getAllTimeZones();
-        System.out.println(allTimeZones);
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now.format(COMMON_FORMATTER));
-        System.out.println(getUtcFormatDateTime(now.format(COMMON_FORMATTER),COMMON_FORMATTER));
-        System.out.println(zoneToUTC(now.format(COMMON_FORMATTER),"Asia/Shanghai",COMMON_FORMATTER));
+    public static String utcToZone(String dateTimeStr,ZoneId targetZone,DateTimeFormatter formatter){
+        return zoneConvert(dateTimeStr,ZoneId.of("UTC"),targetZone,formatter);
     }
+
 }
---
+++ common/src/test/java/com/javademo/utils/DateUtilTest.java
@@ -0,0 +1,85 @@
+package com.javademo.utils;
+
+import com.javademo.common.utils.DateUtil;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class DateUtilTest {
+    private final static Logger LOG = LoggerFactory.getLogger(DateUtilTest.class);
+
+    @Test
+    public void dateFormatTest() {
+        LocalDateTime now = LocalDateTime.now();
+        //时间格式化
+        String commonDateTime = now.format(DateUtil.COMMON_FORMATTER);
+        LOG.info(commonDateTime);
+        ZonedDateTime defaultZone = now.atZone(ZoneId.systemDefault());
+        String defaultZoneDateTime = defaultZone.format(DateUtil.ZONE_TIME_FORMAT);
+        LOG.info(defaultZoneDateTime);
+        ZonedDateTime gmtOffsetZone = now.atZone(ZoneId.of("GMT+8"));
+        String gmtOffsetZoneDateTime = gmtOffsetZone.format(DateUtil.ZONE_TIME_FORMAT);
+        LOG.info(gmtOffsetZoneDateTime);
+        String utcDateTime = now.format(DateUtil.ISO_FORMATTER);
+        LOG.info(utcDateTime);
+        //字符串转时间
+        assert LocalDateTime.parse("2022-07-06 18:39:36:843", DateUtil.COMMON_FORMATTER)
+                .isEqual(LocalDateTime.parse("2022-07-06 18:39:36:843 HKT", DateUtil.ZONE_TIME_FORMAT))
+                &&
+                LocalDateTime.parse("2022-07-06 18:39:36:843", DateUtil.COMMON_FORMATTER)
+                        .isEqual(LocalDateTime.parse("2022-07-06 18:39:36:843 HKT", DateUtil.ZONE_TIME_FORMAT))
+                &&
+                LocalDateTime.parse("2022-07-06 18:39:36:843 HKT", DateUtil.ZONE_TIME_FORMAT)
+                        .isEqual(LocalDateTime.parse("2022-07-06 18:39:36:843 GMT+08:00", DateUtil.ZONE_TIME_FORMAT))
+                &&
+                LocalDateTime.parse("2022-07-06 18:39:36:843 GMT+08:00", DateUtil.ZONE_TIME_FORMAT)
+                        .isEqual(LocalDateTime.parse("2022-07-06T18:39:36.843Z", DateUtil.ISO_FORMATTER));
+    }
+
+    @Test
+    public void zoneConvert() {
+        LocalDateTime now = LocalDateTime.now();
+        ZoneId cnZone = ZoneId.of("GMT+8");
+        ZonedDateTime cnDateTime = now.atZone(cnZone);
+        String cnTime = cnDateTime.format(DateUtil.COMMON_FORMATTER);
+        ZoneId utcZone = ZoneId.of("UTC");
+        String utcDateTime = DateUtil.zoneConvert(cnTime, cnZone, utcZone, DateUtil.COMMON_FORMATTER);
+        LOG.info(cnTime);
+        LOG.info(utcDateTime);
+        assert DateUtil.zoneConvert(utcDateTime, utcZone, cnZone, DateUtil.COMMON_FORMATTER).equals(cnTime);
+    }
+
+    @Test
+    public void zoneToUTC() {
+        LocalDateTime now = LocalDateTime.now();
+        ZoneId cnZone = ZoneId.of("GMT+8");
+        ZonedDateTime cnDateTime = now.atZone(cnZone);
+        String cnTime = cnDateTime.format(DateUtil.COMMON_FORMATTER);
+        String utcDateTime = DateUtil.zoneToUTC(cnTime, cnZone, DateUtil.COMMON_FORMATTER);
+        LOG.info(cnTime);
+        LOG.info(utcDateTime);
+        assert cnTime.equals(DateUtil.utcToZone(utcDateTime, cnZone, DateUtil.COMMON_FORMATTER));
+    }
+
+    @Test
+    public void getISOFormatDateTime() {
+        String isoFormatDateTime = DateUtil.getISOFormatDateTime("2022-07-06 19:05:15:033", DateUtil.COMMON_FORMATTER);
+        assert isoFormatDateTime.equals("2022-07-06T19:05:15.033Z");
+    }
+
+    @Test
+    public void getAllTimeZones() {
+        List<String> allTimeZones = DateUtil.getAllTimeZones();
+        assert allTimeZones != null && !allTimeZones.isEmpty();
+        LOG.info("Available ZoneIds {}", allTimeZones);
+        for (String zoneId :
+                allTimeZones) {
+            assert zoneId != null;
+        }
+    }
+}
