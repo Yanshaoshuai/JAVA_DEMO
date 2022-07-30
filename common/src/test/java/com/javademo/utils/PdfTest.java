3d2d3288ef48b86aa4a9afc02f8e6165c79a2264
yanshaoshuai <ahutyss@gmail.com>
2022-07-30 21:44:25 +0800

    add some test

--- common/src/test/java/com/javademo/utils/PdfTest.java
+++ common/src/test/java/com/javademo/utils/PdfTest.java
@@ -2,13 +2,13 @@ package com.javademo.utils;
 
 
 import com.javademo.common.utils.FreeMarkerUtil;
-import com.javademo.common.utils.GsonUtil;
 import com.javademo.common.utils.PdfUtil;
 import freemarker.template.TemplateException;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
@@ -17,12 +17,22 @@ public class PdfTest {
     private final static Logger LOG = LoggerFactory.getLogger(PdfTest.class);
     @Test
     public void testGenPdfFromTemp(){
-        Map<String, Object> param = Map.of("title", "Colossal", "imgPath", "E:\\File\\code\\JAVA_DEMO\\upload\\colossal.jpg", "imgName", "Colossal");
+        Map<String, Object> param = Map.of("title", "Colossal", "imgPath", "colossal.jpg", "imgName", "Colossal");
         try {
             String html = FreeMarkerUtil.generateHtml("test.html", param);
             LOG.info("html:----------\n {} \n---------------",html);
             PdfUtil.createPdf(html,new FileOutputStream("test.pdf"));
         } catch (IOException | TemplateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void testGenPdfFromTempWithHeaderFooter(){
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("test-footer.pdf");
+            FileInputStream fileInputStream = new FileInputStream("E:\\File\\code\\JAVA_DEMO\\common\\nameddestinations.pdf");
+            PdfUtil.addFooter("",fileOutputStream,fileInputStream);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
